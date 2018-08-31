5047cebecdb74a6d4d29b95e887966e7fcf060f1
Alex <alex.schmid91@gail.com>
2018-08-31 12:25:09 +0200

    Decouple GUI from Engine with PubSub system

--- Interface.py
+++ Interface.py
@@ -1,66 +1,75 @@
-from functools import partial
+from functools import wraps
 
 import wx
+from pubsub.pub import subscribe, sendMessage, addTopicDefnProvider, TOPIC_TREE_FROM_CLASS
 from serial.tools.list_ports import comports
 
+import topic_def
 from Ventolino import Ventolino
+
+addTopicDefnProvider(topic_def, TOPIC_TREE_FROM_CLASS)
+
+
+def main_thread(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        wx.CallAfter(func, *args, **kwargs)
+
+    return wrapper
 
 
 class VentolinoGUI(wx.Frame):
-    def __init__(self, channels, engine=None, *args, **kwargs):
+    def __init__(self, channels, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.engine = engine
         self.channels = channels
-
         self.timer = wx.Timer(self)
+        self.Bind(event=wx.EVT_TIMER, source=self.timer, handler=self.update)
 
-        self.menu_bar = Menubar(style=wx.BORDER_NONE)
+        self.SetBackgroundColour('white')
+
+        self.menu_bar = Menubar(style=wx.BORDER_NONE, timer=self.timer)
         self.SetMenuBar(self.menu_bar)
-
         self.Bind(wx.EVT_MENU, self.on_quit, id=wx.ID_CLOSE)
+        self.Bind(wx.EVT_MENU, self.on_connect, source=self.menu_bar.rod_com_menu.connect)
 
-        panel = wx.Panel(parent=self)
-        if channels == 1:
-            sizer = wx.GridSizer(rows=1, cols=1, vgap=10, hgap=20)
-        elif channels == 2:
-            sizer = wx.GridSizer(rows=1, cols=2, vgap=10, hgap=20)
-        else:
-            sizer = wx.GridSizer(rows=2, cols=2, vgap=10, hgap=20)
+        self.status_bar = wx.StatusBar(parent=self)
+        self.SetStatusBar(self.status_bar)
+        subscribe(listener=self.set_status, topicName='ETG_status')
 
         self.MFC = []
+        if channels == 1:
+            sizer = wx.GridSizer(rows=1, cols=1, vgap=0, hgap=0)
+        elif channels == 2:
+            sizer = wx.GridSizer(rows=1, cols=2, vgap=0, hgap=0)
+        else:
+            sizer = wx.GridSizer(rows=2, cols=2, vgap=0, hgap=0)
+
         for channel in range(channels):
-            self.MFC.append(MFCChannelPanel(parent=panel, channel=channel + 1))
-            sizer.Add(self.MFC[channel])
+            self.MFC.append(MFCChannelPanel(parent=self, channel=channel + 1))
+            sizer.Add(self.MFC[channel], border=5, flag=wx.ALL)
 
-        panel.SetSizerAndFit(sizer)
-
+        sizer.Fit(self)
+        self.SetSizer(sizer)
+        self.SetMinSize(self.GetSize())
         self.Show(True)
-
-    def bind_to_engine(self, engine):
-        if not self.engine:
-            self.engine = engine
-
-        self.menu_bar.rod_com_menu.binding(self.engine, self.timer)
-
-        self.Bind(wx.EVT_TIMER, self.update, self.timer)
-
-        for panel in self.MFC:
-            panel.binding(self.engine)
 
     def on_quit(self, *args):
         self.Close()
 
+    def set_status(self, text=''):
+        wx.CallAfter(lambda: self.status_bar.SetStatusText(text))
+        wx.CallAfter(lambda: wx.CallLater(millis=4000, callableObj=self.status_bar.SetStatusText, text=''))
+
     def update(self, *args):
         for chan in range(self.channels):
-            self.engine.read_is_flow(channel=chan+1)
-            self.MFC[chan].is_label.SetLabel('Flow: {:3.1f}%'.format(self.engine.is_flows[chan]))
-            self.MFC[chan].gauge.SetValue(int(self.engine.is_flows[chan]))
+            sendMessage(topicName='GTE_read_is_flow', channel=chan+1)
+            sendMessage(topicName='GTE_read_set_flow', channel=chan+1)
 
-            self.engine.read_set_flow(channel=chan+1)
-            self.MFC[chan].set_label.SetLabel('Set: {:3.1f}%'.format(self.engine.set_flows[chan]))
+        self.timer.Start(1000)
 
-        self.timer.Start(300)
+    def on_connect(self, *args):
+        self.timer.Start(2000)
 
 
 class MFCChannelPanel(wx.Panel):
@@ -69,13 +78,17 @@ class MFCChannelPanel(wx.Panel):
 
         self.channel = channel
 
+        subscribe(topicName='ETG_read_is_flow', listener=self.update_flow_is_data)
+        subscribe(topicName='ETG_read_set_flow', listener=self.update_flow_set_data)
+
         self.set_button = wx.Button(parent=self, label='Set Flow')
+        self.set_button.Bind(wx.EVT_BUTTON, self.set_flow)
 
         self.flow_control = wx.SpinCtrlDouble(parent=self, value='0', min=0, max=100, inc=0.1, style=wx.SP_ARROW_KEYS,
                                               size=(70, 20))
 
-        self.is_label = wx.StaticText(parent=self, label='Flow:      ')
-        self.set_label = wx.StaticText(parent=self, label='Set:      ')
+        self.is_label = wx.StaticText(parent=self, label='Flow: {: >4.1f}'.format(0))
+        self.set_label = wx.StaticText(parent=self, label='Set: {: >4.1f}'. format(0))
 
         gridsizer = wx.FlexGridSizer(rows=2, cols=2, vgap=10, hgap=20)
         gridsizer.Add(self.is_label, proportion=1, flag=wx.EXPAND | wx.ALIGN_CENTER_VERTICAL)
@@ -92,34 +105,42 @@ class MFCChannelPanel(wx.Panel):
 
         self.SetSizerAndFit(boxsizer)
 
-    def set_flow_handler(self, *args, engine):
+    def set_flow(self, *args):
         flow_value = self.flow_control.GetValue()
-        engine.set_flow(channel=self.channel, flow=flow_value)
+        assert 0.0 <= flow_value <= 100.0, 'Invalid flow percentage'
+        sendMessage(topicName='GTE_set_flow', channel=self.channel, flow=flow_value)
 
-    def binding(self, engine):
-        self.Bind(wx.EVT_BUTTON, partial(self.set_flow_handler, engine=engine), source=self.set_button)
+    def update_flow_is_data(self, channel=1, flow=0):
+        if channel == self.channel:
+            wx.CallAfter(lambda: self.is_label.SetLabel('Flow: {:3.1f}%'.format(flow)))
+            wx.CallAfter(lambda: self.gauge.SetValue(int(flow)))
+
+    def update_flow_set_data(self, channel, flow):
+        if channel == self.channel:
+            wx.CallAfter(lambda: self.set_label.SetLabel('Set: {:3.1f}%'.format(flow)))
 
 
 class Menubar(wx.MenuBar):
-    def __init__(self, _=None, *args, **kwargs):
+    def __init__(self, timer, _=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.port = None
+        self.timer = timer
 
         filemenu = wx.Menu()
         filemenu.Append(item='Quit', id=wx.ID_CLOSE)
 
-        self.rod_com_menu = PortMenu()
+        self.rod_com_menu = PortMenu(timer=self.timer)
 
         self.Append(filemenu, 'File')
         self.Append(self.rod_com_menu, 'Serial Connection')
 
 
 class PortMenu(wx.Menu):
-    def __init__(self):
+    def __init__(self, timer):
         super().__init__()
 
-        self.selected_port = None
+        self.timer = timer
 
         self.portdict = self.port_dict = {port[1]: port[0] for port in comports()}
         self.portItems = [wx.MenuItem(parentMenu=self, id=wx.ID_ANY, text=port, kind=wx.ITEM_RADIO)
@@ -130,21 +151,21 @@ class PortMenu(wx.Menu):
 
         self.AppendSeparator()
 
+        self.refresh = self.Append(id=wx.ID_ANY, item='Refresh')
         self.connect = self.Append(id=wx.ID_ANY, item='Connect', kind=wx.ITEM_CHECK)
+        self.Bind(event=wx.EVT_MENU, handler=self.on_connect, source=self.connect)
 
-    def binding(self, engine, timer):
-        self.Bind(event=wx.EVT_MENU, handler=partial(self.connect_handler, engine=engine, timer=timer), source=self.connect)
-
-    def connect_handler(self, source, engine, timer):
+    def on_connect(self, source):
         if source.IsChecked():
             for item in self.portItems:
                 if item.IsChecked():
-                    engine.connect(self.port_dict[item.GetText()])
-                    timer.Start()
+                    sendMessage(topicName='GTE_connect', port=self.port_dict[item.GetItemLabelText()])
+                    source.Skip()
 
         else:
-            timer.Stop()
-            engine.disconnect()
+            self.timer.Stop()
+            sendMessage(topicName='GTE_disconnect')
+
 
 
 
@@ -152,8 +173,7 @@ class PortMenu(wx.Menu):
 def main():
     ex = wx.App()
     eng = Ventolino()
-    g = VentolinoGUI(parent=None, channels=2)
-    g.bind_to_engine(engine=eng)
+    g = VentolinoGUI(parent=None, channels=4)
     ex.MainLoop()
 
 
--- Ventolino.py
+++ Ventolino.py
@@ -1,50 +1,114 @@
-from serial import Serial
+from threading import Thread, Lock
+
+from pubsub.pub import subscribe, sendMessage
+from serial import Serial, SerialException
 
 
 class Ventolino(Serial):
-    def __init__(self, port=None, channels=4):
-        super().__init__(port, timeout=1)
+    """Driver class for Ventolino MFC controllers, uses Pubsub to communicate with GUI"""
+    def __init__(self, port=None, channels=4, timeout=1):
+        super().__init__(port, timeout=timeout)
         self.port = port
         self.channels = channels
 
-        self.set_flows = [0.0]*4
-        self.is_flows = [0.0]*4
+        self.com_lock = Lock()
+
+        self.set_flows = [0.0]*channels
+        self.is_flows = [0.0]*channels
+
+        subscribe(self.connect, 'GTE_connect')
+        subscribe(self.disconnect, 'GTE_disconnect')
+        subscribe(self.set_flow, 'GTE_set_flow')
+        subscribe(self.read_is_flow, 'GTE_read_is_flow')
+        subscribe(self.read_set_flow, 'GTE_read_set_flow')
+        subscribe(self.read_all_channels_is, 'GTE_read_all_channels_is')
+        subscribe(self.read_all_channels_set, 'GTE_read_all_channels_set')
 
     def connect(self, port=None):
+        """Connect to serial port, emmit success/error message"""
         if port:
             self.port = port
-        self.open()
+        try:
+            self.open()
+            sendMessage(topicName='ETG_status', text='Ventolino connected')
+        except SerialException:
+            sendMessage(topicName='ETG_status', text='Serial connection error')
 
     def disconnect(self):
         self.close()
+        sendMessage(topicName='ETG_status', text='Ventolino disconnected')
 
     def set_flow(self, channel, flow):
         """Set desired flow"""
-        if 0 <= flow <= 100 and 1 <= channel <= 4:
+
+        assert 0 <= flow <= 100 and 1 <= channel <= self.channels, 'Invalid channel or flow'
+
+        def target_func():
             string = '{:02d}SFD{:3.1f}'.format(channel, flow)
-            self.write(b'\x02')
-            self.write(string.encode())
-            self.write(b'\x0D')
+
+            with self.com_lock:
+                try:
+                    self.write(b'\x02')
+                    self.write(string.encode())
+                    self.write(b'\x0D')
+
+                    answer = self.readline()
+                    print(answer)
+                    if answer == b'rec\n':
+                        sendMessage('ETG_status', text='Chnnale {:1d} set to {:4.1f} %.'.format(channel, flow))
+                    else:
+                        sendMessage('ETG_status', text='Ventolino not answering')
+
+                except (ValueError, SerialException):
+                    sendMessage(topicName='ETG_status', text='Serial communication error!')
+
+        com_thread = Thread(target=target_func)
+        com_thread.start()
 
     def read_is_flow(self, channel):
-        if 1 <= channel <= 4:
-            string = '{:02d}RFX'.format(channel)
-            self.write(b'\x02')
-            self.write(string.encode())
-            self.write(b'\x0D')
+        """Read flow, emit message with flow value or status message with error"""
+        assert 1 <= channel <= self.channels, 'Invalid channel'
 
-            answer = float(self.readline().decode())
-            self.is_flows[channel - 1] = answer
+        def target_func():
+            string = '{:02d}RFX'.format(channel)
+
+            with self.com_lock:
+                try:
+                    self.write(b'\x02')
+                    self.write(string.encode())
+                    self.write(b'\x0D')
+
+                    answer = self.readline()
+                    self.is_flows[channel - 1] = float(answer.decode())
+                    sendMessage(topicName='ETG_read_is_flow', channel=channel, flow=self.is_flows[channel - 1])
+
+                except (ValueError, SerialException):
+                    sendMessage(topicName='ETG_status', text='Serial communication error!')
+
+        com_thread = Thread(target=target_func)
+        com_thread.start()
 
     def read_set_flow(self, channel):
-        if 1 <= channel <= 4:
-            string = '{:02d}RFD'.format(channel)
-            self.write(b'\x02')
-            self.write(string.encode())
-            self.write(b'\x0D')
+        assert 1 <= channel <= self.channels, 'Invalid channel'
 
-            answer = float(self.readline().decode())
-            self.set_flows[channel - 1] = answer
+        def target_func():
+            string = '{:02d}RFD'.format(channel)
+
+            with self.com_lock:
+                try:
+                    self.write(b'\x02')
+                    self.write(string.encode())
+                    self.write(b'\x0D')
+
+                    answer = self.readline()
+                    self.set_flows[channel - 1] = float(answer.decode())
+                    sendMessage(topicName='ETG_read_set_flow', channel=channel, flow=self.set_flows[channel - 1])
+
+                except (ValueError, SerialException):
+                    sendMessage(topicName='ETG_status', text='Serial communication error!')
+
+        com_thread = Thread(target=target_func)
+        com_thread.start()
 
     def read_all_channels_is(self):
         for channel in range(self.channels):
@@ -53,3 +117,17 @@ class Ventolino(Serial):
     def read_all_channels_set(self):
         for channel in range(self.channels):
             self.read_set_flow(channel + 1)
+
+
+class SerialIOThread(Thread):
+    """Thread for handling serial communication"""
+    def __init__(self, command, param_dict=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.command = command
+        self.param = param_dict
+
+    def run(self):
+        if self.param:
+            self.command(**self.param)
+        else:
+            self.command()
